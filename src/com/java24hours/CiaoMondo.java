762081ec1eb0b236ef1499fc6c17840acbce97c7
Roberto Nogueira <enogrob@gmail.com>
2016-06-06 16:02:10 -0300

    Hour3 performed

--- src/com/java24hours/CiaoMondo.java
+++ src/com/java24hours/CiaoMondo.java
@@ -1,3 +1,5 @@
+package com.java24hours;
+
 class CiaoMondo {
     public static void main(String[] args) {
         String greeting = "Ciao mondo!";
---
+++ src/com/java24hours/Saluton.java
@@ -0,0 +1,11 @@
+package com.java24hours;
+
+/**
+ * Created by enogrob on 6/6/16.
+ */
+public class Saluton {
+    public static void main (String[] args) {
+        String greeting = "Hello World!";
+        System.out.println(greeting);
+    }
+}
