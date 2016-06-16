58d4ef99edbf9fffd1965e165330165bfdc0c1cb
Roberto Nogueira <enogrob@gmail.com>
2016-06-15 21:49:09 -0300

    Performed Hour05

---
+++ src/com/java24hours/exercises/Hour05Exercise2.java
@@ -0,0 +1,12 @@
+package com.java24hours.exercises;
+
+/**
+ * Created by enogrob on 6/15/16.
+ */
+public class Hour05Exercise2 {
+    public static void main(String[] args) {
+        int x = 4;
+        int y = 4;
+        System.out.println("Result = " + (Math.sqrt(x) + Math.sqrt(y)));
+    };
+}
