8a89715250b69a3d5220d18900ef37bcc40ca644
Roberto Nogueira <enogrob@gmail.com>
2016-06-22 00:04:19 -0300

    Performed Hour06

---
+++ src/com/java24hours/exercises/Favorite.java
@@ -0,0 +1,10 @@
+class Favorite {
+    public static void main(String[] arguments) {
+        // set up film information
+        String favorite = "chainsaw";
+        String guess = "chainsaw";
+        System.out.println("Is Fin's favorite weapon  a "
+                + guess + "?");
+        System.out.println("Answer: " + favorite.equals(guess));
+    }
+}
